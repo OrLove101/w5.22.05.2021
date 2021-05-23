d3534c033ed32631f73a286d9b4af9ac870a0d85
OrLove101 <danyazerorebel@gmail.com>
2021-05-23 22:28:39 +0300

    docs: complit generators

--- src/generators.py
+++ src/generators.py
@@ -5,6 +5,53 @@ def ari_prog(start, stop, step):
         start += step
 
 
+def geo_prog(start, stop, multiplier):
+    while start < stop:
+        yield start
+
+        start *= multiplier
+
+
+def fibonacci_prog(start, stop):
+    while start < stop:
+        flag = 1
+        second = 1
+        first = 0
+        counter = 1
+        yielded = False
+
+        if start == 1 or start == -1:
+            yield 1
+            yielded = True
+            start += 1
+        if -2 < start < 2 and not yielded:
+            yield start
+            yielded = True
+            start += 1
+        if start < 0 and not yielded:
+            if start % 2 == 0:
+                flag = -1
+            start *= -1
+        while counter < start:
+            next = first + second
+            first = second
+            second = next
+            counter += 1
+        yield next*flag
+        start += 1
+
+
+def factorial_prog(start, stop):
+    while start < stop:
+        yield factorial(start)
+
+        start += 1
+
+
+def factorial(value):
+    return 1 if value < 2 else value * factorial(value - 1)
+
+
 if __name__ == '__main__':  # pragma: no cover
-    for i in ari_prog(0, 10, 2):
+    for i in factorial_prog(1, 10):
         print(i)
--- tests/test_generators/test_generators.py
+++ tests/test_generators/test_generators.py
@@ -1,11 +1,44 @@
 import pytest
 
-from generators import ari_prog
+from src.generators import ari_prog, geo_prog, fibonacci_prog, factorial_prog
 
 
 def test_ari_prog():
     generator = ari_prog(0, 10, 2)
     expected_results = [0, 2, 4, 6, 8]
+
+    for result in expected_results:
+        assert next(generator) == result
+
+    with pytest.raises(StopIteration):
+        next(generator)
+
+
+def test_geo_prog():
+    generator = geo_prog(1, 10, 2)
+    expepted_results = [1, 2, 4, 8]
+
+    for result in expepted_results:
+        assert next(generator) == result
+
+    with pytest.raises(StopIteration):
+        next(generator)
+
+
+def test_fibonacci_prog():
+    generator = fibonacci_prog(1, 10)
+    expected_results = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+    for result in expected_results:
+        assert next(generator) == result
+
+    with pytest.raises(StopIteration):
+        next(generator)
+
+
+def test_factorial_prog():
+    generator = factorial_prog(1, 10)
+    expected_results = [1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
 
     for result in expected_results:
         assert next(generator) == result
